a6e4c0018732e6fa32fdd4bf034a6a12d06ab8f1
carlosSimplicio <carlos.edsmoura@gmail.com>
2025-05-18 01:19:52 -0300

    migrations: set columns to not null

---
+++ backend/db/migrations/000005_alter_column_employee_shift_start_end_time.down.sql
@@ -0,0 +1,3 @@
+
+ALTER TABLE employee_shift ALTER COLUMN start_time DROP NOT NULL;
+ALTER TABLE employee_shift ALTER COLUMN end_time DROP NOT NULL;
---
+++ backend/db/migrations/000005_alter_column_employee_shift_start_end_time.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE employee_shift ALTER COLUMN start_time SET NOT NULL;
+ALTER TABLE employee_shift ALTER COLUMN end_time SET NOT NULL;
